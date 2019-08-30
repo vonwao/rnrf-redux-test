b251a5c51b81fa976999e09f9f077b4a5a09195f
Otto von Wachter <vonwao@gmail.com>
2019-08-30 12:15:31 -0700

    get up and running

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Actions, Router, Scene} from "react-native-router-flux";
+import Home from "./src/home";
+import Page from "./src/page";
+import {connect, Provider} from "react-redux";
+import {createStore} from "redux";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const navigator = Actions.create(<Scene key="root" hideNavBar>
+  <Scene key="home" component={Home}/>
+  <Scene key="page" component={Page}/>
+</Scene>);
+
+const ReduxRouter = connect((state) => ({ state: state.route }))(Router);
+// it is important to load reducers AFTER actions.create (so no import here)
+const reducers = require('./src/reducers').default;
+
+export default
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={createStore(reducers, {})}>
+        <ReduxRouter navigator={navigator} />
+      </Provider>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
